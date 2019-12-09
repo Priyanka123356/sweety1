d88780ce6c9e41996bcbac7ae7f2edbb5607ccae
Priyanka123356 <57991335+Priyanka123356@users.noreply.github.com>
2019-12-09 17:04:29 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,10 @@
+package com.java2novice.junit.samples;
+ public class MyEvenOdd {
+    public boolean isEvenNumber(int number){
+         boolean result = false;
+         if(number%2 == 0){
+            result = true;
+        }
+        return result;
+    }
+}
