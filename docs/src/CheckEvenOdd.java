ceb355d65317fd4a5fa9ff5e728aa0aeafaf7a7e
Priyanka123356 <57991335+Priyanka123356@users.noreply.github.com>
2019-12-11 12:55:27 +0530

    Update CheckEvenOdd.java

--- docs/src/CheckEvenOdd.java
+++ docs/src/CheckEvenOdd.java
@@ -1,1 +1,11 @@
-
+class CheckEvenOdd
+{
+  public static void main(String args[])
+  {
+    int num=4;
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+  }
+}
